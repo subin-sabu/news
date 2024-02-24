3bd998444ba86d3a20c9d6ff09ced367d974d82a
subin-sabu <subincs2023@gmail.com>
2024-02-25 01:16:29 +0530

    made NewsCard responsive and taking data from json and making cards; NewsCardXS is still untouched

--- src/Components/NewsCard/NewsCard.jsx
+++ src/Components/NewsCard/NewsCard.jsx
@@ -9,7 +9,30 @@ import { Container } from '@mui/system';
 
 
 
-const createNewsCard = [1, 2, 3, 4]
+const newsArray = [{
+  imageURL: 'https://img.manoramanews.com/content/dam/mm/mnews/news/breaking-news/images/2024/2/23/Byju-Raveendran23.jpg.image.624.325.jpg',
+  time: '3 hours ago',
+  title: '‘ബൈജൂസി’ല്‍ അസാധാരണ ജനറല്‍ ബോഡി; ബൈജുവിനെ പുറത്താക്കാന്‍ വോട്ട്...',
+  description: 'ബൈജൂസ് ആപ്പില്‍ നിന്ന് ഉടമ ബൈജു രവീന്ദ്രനെ പുറത്താക്കാനുറച്ച് ഓഹരി ഉടമകള്‍...'
+},
+{
+  imageURL: 'https://img.manoramanews.com/content/dam/mm/mnews/news/india/images/2022/10/4/election-commission-of-india.jpg.image.624.325.jpg',
+  time: '6 hours ago',
+  title: 'ലോക്സഭാ തിരഞ്ഞെടുപ്പ് പ്രഖ്യാപനം മാര്‍ച്ച് 13നോ അതിനുശേഷമോ ഉണ്ടായേക്കും..',
+  description: 'ലോക്സഭാ തിരഞ്ഞെടുപ്പ് പ്രഖ്യാപനം മാര്‍ച്ച് 13നോ അതിന് ശേഷമോ ഉണ്ടായേക്കും...'
+},
+{
+  imageURL: 'https://www.manoramanews.com/content/dam/mm/mnews/news/india/images/2023/11/7/high%20court.jpg.image.845.440.jpg',
+  time: '1 hour ago',
+  title: 'സോഷ്യൽ ഫോറസ്ട്രിക്ക് ഓഫീസ്; മരം വെട്ടാനുള്ള നീക്കം ഹൈക്കോടതി തടഞ്ഞു',
+  description: 'കൊച്ചിയിൽ സോഷ്യൽ ഫോറസ്ട്രി വിഭാഗത്തിന് ഓഫീസ് നിർമ്മിക്കാൻ മരം വെട്ടാനുള്ള നീക്കം ഹൈക്കോടതി തടഞ്ഞു'
+},
+{
+  imageURL: 'https://www.manoramanews.com/content/dam/mm/mnews/news/entertainment/images/2024/2/21/bramayugam-premalu.jpg.image.845.440.jpg',
+  time: '10 hours ago',
+  title: 'തിയറ്റര്‍ നിറച്ച് ‘പ്രേമയുഗം’ ; കോടിക്കിലുക്കത്തില്‍ മലയാള സിനിമ',
+  description: 'ഭ്രമയുഗവും പ്രേമലുവും; 2024ലെ ആദ്യ വമ്പന്‍ ഹിറ്റുകള്‍, വീണ്ടും തിയറ്റര്‍ ആരവങ്ങളിലേക്ക് പ്രേക്ഷകരെ ത..'
+}];
 
 
 export default function NewsCard() {
@@ -20,25 +43,25 @@ export default function NewsCard() {
         columnSpacing={{ xs: 1, sm: 2, md: 3, lg: 4 }}
         justifyContent='center'>
 
-        {createNewsCard.map((createNewsCard) => (
-          <Grid item key={createNewsCard} xs={12} sm={6} md={4} lg={3}   >
-            <Card sx={{ maxWidth: 345, mx: 'auto' }}>
-              <CardActionArea>
+        {newsArray.map((news, index) => (
+          <Grid item key={index} xs={12} sm={6} md={4} lg={3}   >
+            <Card sx={{  maxWidth: 345, height: '100%', display: 'flex', flexDirection: 'column', justifyContent: 'space-between'  }}>
+              <CardActionArea sx={{ height: '100%', display: 'flex', flexDirection: 'column', justifyContent: 'space-between' }}>
                 <CardMedia
                   component="img"
                   height="140"
-                  image="https://img.manoramanews.com/content/dam/mm/mnews/news/kerala/images/2024/2/16/Mozha.jpg.image.260.136.jpg"
-                  alt="news"
+                  image={news.imageURL}
+                  alt="news image"
                 />
-                <CardContent>
+                <CardContent sx={{ height: '100%', display: 'flex', flexDirection: 'column', justifyContent: 'space-between' }}>
                   <Typography gutterBottom variant="caption" color='text.secondary' component="div">
-                    05:25 hours ago
+                    {news.time}
                   </Typography>
-                  <Typography gutterBottom variant="h6" component="div">
-                    ആരാണ് മോഴയാനകള്‍? കൊമ്പനോ അതോ പിടിയോ?...
+                  <Typography gutterBottom variant="h6" component="div" sx={{ mb: 1 }}>
+                    {news.title}
                   </Typography>
-                  <Typography variant="body2" color="text.secondary">
-                    കേരളം മുഴുവൻ കുറച്ചു ദിവസമായി ഒരു കൊലയാളി ആനയ്ക്ക് പുറകേയാണ്. ബേലൂർ മഖ്ന എന്ന മോഴയാന. ഇപ്പോഴിതാ...
+                  <Typography variant="body2" color="text.secondary" sx={{ overflow: 'hidden', textOverflow: 'ellipsis', display: '-webkit-box', WebkitLineClamp: '3', WebkitBoxOrient: 'vertical' }}>
+                    {news.description}
                   </Typography>
                 </CardContent>
               </CardActionArea>
