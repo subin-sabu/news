2a2c7c65e97c4d2650c6f16cfa17d46346a4f88f
subin-sabu <subincs2023@gmail.com>
2024-03-22 08:21:39 +0530

    arranged home view . Need to check memory- too many components

--- src/Advertisements/HomeAd16x9.jsx
+++ src/Advertisements/HomeAd16x9.jsx
@@ -6,7 +6,7 @@ function HomeAd16x9({className}) {
   return (
     <div className={className}>
       <Link to='https://www.keralavisionisp.com/search-plans' alt='ad link' style={{textDecoration:'none', color:'inherit'}}>
-      <img style={{aspectRatio:"16/9", maxWidth:'100%'}} src={ad} alt="home ad2 16*9" />
+      <img style={{aspectRatio:"16/9", maxWidth:'100%', marginTop:'1rem'}} src={ad} alt="home ad2 16*9" />
       </Link>
     </div>
   )
