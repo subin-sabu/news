8b822a7e831b3f24632ca6029ab67dd086ee3dd3
subin-sabu <subincs2023@gmail.com>
2024-03-22 09:56:48 +0530

    arranged home grid. no left intendation in mobile view now

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -16,14 +16,16 @@ function Home() {
         <div className={styles['grid-container']}>
 
           <NewsCardVertical startIndex={1} endIndex={7} heading='Latest News' className={styles.snippet1}/>
-          <NewsCardVertical startIndex={1} endIndex={6} heading='Latest News' className={styles.snippet1reduced}/>
+          
 
           <MainNews className={styles.main} />
           <NewsCard className={styles.newsCard} startIndex={0} endIndex={1} imageType="image"/>
 
           <NewsCardVertical startIndex={7} endIndex={13} heading='Must Read' className={styles.snippet2} />
-          
-         
+          <div className={styles.newsCardTab} style={{marginTop:'1rem', backgroundColor:'#DBDBDB', padding:'1rem'}}>
+          <NewsCard  startIndex={7} endIndex={13} />
+          </div>
+
           <HomeAd16x9  className={styles.ad2} />
           
 
